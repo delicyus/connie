02e0db214e3dd3c1e996c7fcdec57ca10add1e0e
delicyus web <delicyus@delicyus.com>
2022-02-11 11:15:50 +0100

    Fichiers commentés des 3 solutions

---
+++ solution 3.php
@@ -0,0 +1,36 @@
+<?php
+// SOLUTION 3 : On ajoute une fonction pour que wp_mail traite le HTML
+// A placer apres la function courrielEnvoyer()
+
+
+// // HELPER Support pour API custom routes
+// // Permet d'envoyer l'e-mail
+// if (!function_exists('courrielEnvoyer')) {
+//     function courrielEnvoyer($inputs)
+//     {
+//         // Destinataire
+//         //$multiple_to_recipients =  get_bloginfo( 'admin_email' );
+//         $recp =  get_userdata(2);
+//         if ($recp)
+//             $recp_email = $recp->user_email;
+//         // sujet
+//         $the_subject     = "Message from: " . $inputs['prenom'] . " " . $inputs['nom'];
+
+//         // body de l'email
+//         $the_body_html .= nl2br("Sent by: " . $inputs['courriel'] . "\r\n");
+//         $the_body_html .= nl2br("Message: \r\n" . $inputs['message']);
+
+//         // envoi
+//         if (function_exists('wp_mail'))
+//             $wp_mail = wp_mail($recp_email,  $the_subject, $the_body_html);
+
+//         if ($wp_mail)
+//             return "success";
+
+//         return $recp_email;
+//     }
+// }
+add_filter('wp_mail_content_type', create_function('', 'return "text/html"; '));
+
+
+?>
